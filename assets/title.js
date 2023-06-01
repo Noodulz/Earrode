4eb897b6da21bea6eb25566557c6bf997ca47c4f
Jocelyn <28810573+Noodulz@users.noreply.github.com>
2023-06-01 02:50:40 -0400

    Update title.js

--- assets/title.js
+++ assets/title.js
@@ -4,7 +4,7 @@ import {StyleSheet, Text, View} from 'react-native';
 const Title = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Earode</Text>
+      <Text style={styles.title}>Earrode</Text>
     </View>
   );
 };
